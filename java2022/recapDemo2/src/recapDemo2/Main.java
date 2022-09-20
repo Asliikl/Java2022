524b069e6098ee510da301c7c2fb8c14eb5ab8af
Aslıhan İkiel <108464174+Asliikl@users.noreply.github.com>
2022-09-20 12:31:18 +0300

    Java2022 work-1
    Homework

---
+++ java2022/recapDemo2/src/recapDemo2/Main.java
@@ -0,0 +1,44 @@
+package recapDemo2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//**sayıları sırasıyla yazdırma**
+//		double[] myList = {1.2,1.3,4.3,5.6};
+//		
+//		for(double number:myList) {
+//			System.out.println(number);
+//		}
+		
+		
+		//**sayıları toplamı ile yazdırma**
+//		double[] myList = {1.2,1.3,4.3,5.6};
+//		
+//		double total = 0;
+//		for(double number:myList) {
+//			total = total + number;
+//			System.out.println(number);
+//		}
+//		System.out.println("Toplam =" + total);
+//		
+		
+		//** en büyük sayiyi yazdirma
+		double[] myList = {1.2,1.3,4.3,5.6};
+		
+		double total = 0;
+		double max = myList[0];
+		for(double number:myList) {
+			if (max<number) {
+				max = number;
+			}
+			
+			total = total + number;
+			System.out.println(number);
+		}
+		System.out.println("Toplam =" + total);
+		System.out.println("En büyük = " + max);
+		
+	}
+
+}
---
+++ java2022/veriables/src/veriables/main.java
@@ -0,0 +1,17 @@
+package veriables;
+
+public class main {
+
+	//reusability tek sefer yaz ve kullan 
+	
+	public static void main(String[] args) {
+		int ogrenciSayisi =12;
+		String mesaj = "Öğrenci sayısı : ";
+		
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println(mesaj + ogrenciSayisi);
+	}
+
+}
